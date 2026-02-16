79918c36f08ca05101b23c12e056b68c37af1cb7
glob41 <vish1q1q@gmail.com>
2026-02-16 20:26:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,53 +1,68 @@
-const express = require("express");
-const axios = require("axios");
-const cors = require("cors");
+import express from "express";
+import axios from "axios";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
+
 const PORT = process.env.PORT || 10000;
 
-// Allow all origins (for GitHub Pages)
-app.use(cors({
-  origin: "*"
-}));
+// Upstox Tokens
+const TOKEN = process.env.UPSTOX_ACCESS_TOKEN;
 
-const ACCESS_TOKEN = process.env.UPSTOX_ACCESS_TOKEN;
 
-// Test Route
-app.get("/", (req, res) => {
+// Instruments
+const symbols = [
+  "NSE_INDEX|Nifty 50",
+  "NSE_INDEX|Nifty Bank",
+  "NSE_INDEX|Nifty Fin Service",
+  "BSE_INDEX|SENSEX",
+
+  "MCX_FO|GOLD",
+  "MCX_FO|SILVER",
+  "MCX_FO|CRUDEOIL",
+  "MCX_FO|NATURALGAS"
+];
+
+
+// Home
+app.get("/",(req,res)=>{
   res.send("Server Running 🚀");
 });
 
-// Live Market Route
-app.get("/live", async (req, res) => {
-  try {
 
-    if (!ACCESS_TOKEN) {
-      return res.json({ error: "Access Token Missing" });
-    }
+// Live Data
+app.get("/live", async (req,res)=>{
 
-    const response = await axios.get(
-      "https://api.upstox.com/v2/market-quote/ltp?instrument_key=NSE_INDEX|Nifty%2050",
-      {
-        headers: {
-          Authorization: `Bearer ${ACCESS_TOKEN}`,
-          "Api-Version": "2.0"
-        }
+  try{
+
+    let url = `https://api.upstox.com/v2/market-quote/quotes?symbol=${symbols.join(",")}`;
+
+    let response = await axios.get(url,{
+      headers:{
+        Authorization:`Bearer ${TOKEN}`
       }
-    );
-
-    res.json(response.data);
-
-  } catch (err) {
-
-    console.log(err.message);
+    });
 
     res.json({
-      error: "Upstox API Error",
-      details: err.message
+      status:"success",
+      data: response.data.data
     });
+
   }
+  catch(err){
+
+    res.json({
+      status:"error",
+      message:"Upstox API Error"
+    });
+
+  }
+
 });
 
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+
+// Start
+app.listen(PORT,()=>{
+  console.log("Server running on",PORT);
 });
