293bea82aafcdc52d145060116b9ed143c7232b7
glob41 <vish1q1q@gmail.com>
2026-04-10 14:56:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 
+const chart = LightweightCharts.createChart(document.getElementById('chart'), {
+  width: window.innerWidth,
+  height: 400,
+});
+
+const candleSeries = chart.addCandlestickSeries();
+
+fetch("https://your-render-api-url")
+.then(res => res.json())
+.then(data => {
+
+  const formattedData = data.map(item => ({
+    time: item.time,
+    open: item.open,
+    high: item.high,
+    low: item.low,
+    close: item.close
+  }));
+
+  candleSeries.setData(formattedData);
+});
