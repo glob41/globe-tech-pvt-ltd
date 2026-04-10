f0c6d3be78b41411a35101fc002073687efdceb6
glob41 <vish1q1q@gmail.com>
2026-04-10 14:46:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,68 +1,21 @@
-import express from "express";
-import axios from "axios";
-import cors from "cors";
+function showChart(name){
+  let symbol = "NSE:NIFTY";
 
-const app = express();
-app.use(cors());
+  if(name === "BANKNIFTY") symbol = "NSE:BANKNIFTY";
+  if(name === "FINNIFTY") symbol = "NSE:FINNIFTY";
+  if(name === "SENSEX") symbol = "BSE:SENSEX";
 
-const PORT = process.env.PORT || 10000;
+  document.getElementById("chartBox").style.display = "block";
+  document.getElementById("tv_chart").innerHTML = "";
 
-// Upstox Tokens
-const TOKEN = process.env.UPSTOX_ACCESS_TOKEN;
-
-
-// Instruments
-const symbols = [
-  "NSE_INDEX|Nifty 50",
-  "NSE_INDEX|Nifty Bank",
-  "NSE_INDEX|Nifty Fin Service",
-  "BSE_INDEX|SENSEX",
-
-  "MCX_FO|GOLD",
-  "MCX_FO|SILVER",
-  "MCX_FO|CRUDEOIL",
-  "MCX_FO|NATURALGAS"
-];
-
-
-// Home
-app.get("/",(req,res)=>{
-  res.send("Server Running 🚀");
-});
-
-
-// Live Data
-app.get("/live", async (req,res)=>{
-
-  try{
-
-    let url = `https://api.upstox.com/v2/market-quote/quotes?symbol=${symbols.join(",")}`;
-
-    let response = await axios.get(url,{
-      headers:{
-        Authorization:`Bearer ${TOKEN}`
-      }
-    });
-
-    res.json({
-      status:"success",
-      data: response.data.data
-    });
-
-  }
-  catch(err){
-
-    res.json({
-      status:"error",
-      message:"Upstox API Error"
-    });
-
-  }
-
-});
-
-
-// Start
-app.listen(PORT,()=>{
-  console.log("Server running on",PORT);
-});
+  new TradingView.widget({
+    width: "100%",
+    height: 400,
+    symbol: symbol,
+    interval: "1",
+    timezone: "Asia/Kolkata",
+    theme: "dark",
+    style: "1",
+    container_id: "tv_chart"
+  });
+}
